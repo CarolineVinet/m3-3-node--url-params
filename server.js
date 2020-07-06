f66841a29922cd8ce89e29012367d2b0c8573765
Caroline Vinet <carovinet@gmail.com>
2020-07-06 13:21:13 -0400

    exercise 1.1

--- server.js
+++ server.js
@@ -15,7 +15,11 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.set("view engine", "ejs");
 
-// endpoints here
+app.get("/top50", (req, res) => {
+  res.render("pages/top50", {
+    title: "Top 50 Songs Streamed on Spotify",
+  });
+});
 
 // handle 404s
 app.get("*", (req, res) => {
